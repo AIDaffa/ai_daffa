4be771e77711e8fb2dc485a90f329da605d531b1
AIDaffa <alifikhsandaffa87@gmail.com>
2021-10-01 21:47:30 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan</h1>";
+		echo"Nama saya Alif ikhsa D<br> 
+		saya tinggal di Cilacap<br>
+		Saya sangat menyukai Anime";
+	}
+} ?>
